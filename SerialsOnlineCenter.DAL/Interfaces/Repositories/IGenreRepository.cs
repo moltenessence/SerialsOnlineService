baf9ec8e894194e8a599c11396721bfc67378f81
Daniil Feoktistov <whatdoessence@gmail.com>
2022-09-12 01:17:47 +0300

    Add Genre repository

---
+++ SerialsOnlineCenter.DAL/Interfaces/Repositories/IGenreRepository.cs
@@ -0,0 +1,10 @@
+﻿using SerialsOnlineCenter.DAL.Entities;
+
+namespace SerialsOnlineCenter.DAL.Interfaces.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task<GenreEntity> GetById(int id, CancellationToken cancellationToken);
+        Task<IReadOnlyList<GenreEntity>> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ SerialsOnlineCenter.DAL/Repositories/GenreRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using SerialsOnlineCenter.DAL.Entities;
+using SerialsOnlineCenter.DAL.Interfaces.Repositories;
+using static SerialsOnlineCenter.DAL.Helpers.RepositoryHelper;
+
+namespace SerialsOnlineCenter.DAL.Repositories
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly string _connectionString;
+
+        public GenreRepository()
+        {
+            _connectionString = ConnectionString;
+        }
+
+        public async Task<GenreEntity> GetById(int id, CancellationToken cancellationToken)
+        {
+            await using var connection = new MySqlConnection(_connectionString);
+
+            var query = "SELECT FROM Genres WHERE id = @Id";
+
+            var command = CreateCommand(query, new { @Id = id }, cancellationToken: cancellationToken);
+
+            var result = await connection.QuerySingleOrDefaultAsync<GenreEntity>(command);
+
+            return result;
+        }
+
+        public async Task<IReadOnlyList<GenreEntity>> GetAll(CancellationToken cancellationToken)
+        {
+            await using var connection = new MySqlConnection(_connectionString);
+
+            var result = await connection.QueryAsync<GenreEntity>("SELECT * FROM Genres", cancellationToken);
+
+            return result.ToList();
+        }
+    }
+}
+
