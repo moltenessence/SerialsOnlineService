96cbd39f658c57ed2700217340e03554ee4b648c
Daniil Feoktistov <whatdoessence@gmail.com>
2022-12-01 19:30:11 +0300

    make purchase button on subscriptions page

--- SerialsOnlineService.Web/react-client/src/Components/Subscriptions.tsx
+++ SerialsOnlineService.Web/react-client/src/Components/Subscriptions.tsx
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dispatch, bindActionCreators } from "redux";
 import { RootState } from "../redux/store";
 import { ReactJSXIntrinsicAttributes } from '@emotion/react/types/jsx-namespace';
 import * as subscriptionsActionCreators from '../redux/Subscriptions/actionCreators'
-import { getIsFetching, getSubscriptions  } from '../redux/Subscriptions/selectors';
+import { getIsFetching, getSubscriptions } from '../redux/Subscriptions/selectors';
 import Preloader from './other/Preloader';
 import { connect } from 'react-redux';
 import { ISubscription } from '../Common/Models/ISubscription';
-import {SubscriptionItem, SubscriptionsWrapper} from "./styles/Subscriptions.style";
+import { SubscriptionItem, SubscriptionsWrapper } from "./styles/Subscriptions.style";
+import * as purchasesActionCreators from '../redux/Purchases/actionCreators'
+import { IMakePurchaseRequest } from '../Common/Requests/IMakePurchaseRequest';
+import PurchaseButton from './other/PurchaseButton';
+import PurchaseForm from './forms/PurchaseForm';
 
 function mapStateToProps(state: RootState) {
     return {
@@ -17,12 +21,15 @@ function mapStateToProps(state: RootState) {
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-    fetchSubscriptions : subscriptionsActionCreators.fetchSubscriptions,
+    fetchSubscriptions: subscriptionsActionCreators.fetchSubscriptions,
+    makePurchase: purchasesActionCreators.makePurchase
 }, dispatch);
 
 type SerialsProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps> & ReactJSXIntrinsicAttributes
 
-const Serials: React.FC<SerialsProps> = ({ subscriptions, isFetching, fetchSubscriptions }) => {
+const Serials: React.FC<SerialsProps> = ({ subscriptions, isFetching, fetchSubscriptions, makePurchase }) => {
+
+    const [isFormOpened, setFormOpened] = useState(false);
 
     useEffect(() => {
         fetchSubscriptions();
@@ -30,10 +37,12 @@ const Serials: React.FC<SerialsProps> = ({ subscriptions, isFetching, fetchSubsc
 
     const subscriptionsList = subscriptions.map((subscription: ISubscription) => {
         return (
-                <SubscriptionItem key={subscription.id}>
-                    <h3>{subscription.name}</h3>
-                    <p>Price: {subscription.pricePerMonth}$</p>
-                </SubscriptionItem>
+            <SubscriptionItem key={subscription.id}>
+                <h3>{subscription.name}</h3>
+                <p>Price: {subscription.pricePerMonth}$</p>
+                <PurchaseButton callback={() => { setFormOpened(!isFormOpened); }} />
+                {isFormOpened && <PurchaseForm subscriptionId={subscription.id} makePurchase={makePurchase}/>}
+            </SubscriptionItem>
         );
     });
 
