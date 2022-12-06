e27986ade8408454b4c2b0b3d10e04126632cc2f
Daniil Feoktistov <whatdoessence@gmail.com>
2022-12-07 00:22:01 +0300

    Add serials for migration

--- SerialsOnlineCenter.DAL/Migrations/Migration_1109202200001.cs
+++ SerialsOnlineCenter.DAL/Migrations/Migration_1109202200001.cs
@@ -51,7 +51,7 @@ namespace SerialsOnlineCenter.DAL.Migrations
                       "(Targaryen, Lannisters, Starks, Tyrell, Martell, Greyjoys, Baratheons and Boltons) scramble bitterly to gain " +
                       "power over the seven kingdoms and the Iron throne.",
                     release_year = 2011,
-                    genre = "Drama, Fantasy",
+                    genre = "Fantasy",
                     subscription_id = 1
                 })
                 .Row(new
@@ -76,7 +76,45 @@ namespace SerialsOnlineCenter.DAL.Migrations
                     release_year = 2011,
                     genre = "Horror",
                     subscription_id = 2
+                })
+                 .Row(new
+                 {
+                     serial_id = 4,
+                     name = "Friends",
+                     amount_of_series = 236,
+                     description = "The lives of twenty-something friends in New York City, each with their own dreams and drives: Joey a struggling actor, " +
+                     "Monica a chef, Rachel a waitress who hopes to work in fashion, Ross a paleontologist, " +
+                     "Chandler who hates his job in data processing, and Phoebe a masseuse/musician",
+                     release_year = 1994,
+                     genre = "Drama",
+                     subscription_id = 3
+                 })
+                  .Row(new
+                  {
+                      serial_id = 5,
+                      name = "Attack on Titans",
+                      amount_of_series = 87,
+                      description = "Set in a post-apocalyptic world where the remains of humanity live behind walls protecting them from giant humanoid Titans, " +
+                      "Attack on Titan follows protagonist Eren Jaeger, along with friends Mikasa Ackerman and Armin Arlert. When a Colossal Titan breaches the wall of their hometown," +
+                      " Titans destroy the city and eat Eren's mother.",
+                      release_year = 2013,
+                      genre = "Anime",
+                      subscription_id = 4
+                  })
+                .Row(new
+                {
+                    serial_id = 6,
+                    name = "Stranger things",
+                    amount_of_series = 32,
+                    description = "American science fiction horror drama television series created by the Duffer Brothers, who also serve as showrunners and are executive producers along " +
+                    "with Shawn Levy and Dan Cohen. Produced by Monkey Massacre Productions and Levy's 21 Laps Entertainment, " +
+                    "the first season was released on Netflix on July 15, 2016. Its second, third, and fourth seasons followed in October 2017, July 2019, " +
+                    "and May and July 2022, respectively. In February 2022, the series was renewed for a fifth and final season.",
+                    release_year = 2016,
+                    genre = "Drama",
+                    subscription_id = 1
                 });
+
         }
 
         public override void Down()
