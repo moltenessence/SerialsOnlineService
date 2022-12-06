aee4280f433f4f9869cfc4f8260ca1c65f9db067
Daniil Feoktistov <whatdoessence@gmail.com>
2022-12-06 23:15:49 +0300

    fix filter form

--- SerialsOnlineService.Web/react-client/src/Components/forms/SerialsFilterForm.tsx
+++ SerialsOnlineService.Web/react-client/src/Components/forms/SerialsFilterForm.tsx
@@ -3,52 +3,52 @@ import { ISerialsQueryFilter } from "../../Common/Models/ISerialsQueryFilter";
 import { FormWrapper, FormItem, Button, SearchForm } from "../styles/Form.style";
 
 type Props = {
-    filterData: any;
+  filterData: any;
 }
 
 const SerialsFilterForm: React.FC<Props> = ({ filterData }) => {
   return (
     <FormWrapper>
       <Formik
-        initialValues={{ orderByAmountOfSeriesDesc: true, orderByReleaseDesc: false}}
-        onSubmit={(values : ISerialsQueryFilter, { setSubmitting }) => {
-            filterData(values);
+        initialValues={{ orderByAmountOfSeriesDesc: false, orderByReleaseDesc: false }}
+        onSubmit={(values: ISerialsQueryFilter, { setSubmitting }) => {
+          filterData(values);
         }}
       >
         {({ errors, touched }) => (
           <Form>
             <SearchForm>
-            <FormItem>
-              <div>Name (exact name): </div>
-              <Field type="text" name="name" />
-            </FormItem>
-            <FormItem>
-              <div>Release year: </div>
-              <Field type="number" name="releaseYear" Min={0}/>
-            </FormItem>
-            <FormItem>
-              <div>Amount of series: </div>
-              <Field type="number" name="amountOfSeries" Min={0}/>
-            </FormItem>
-            <FormItem>
-              <div>Order by release </div>
-              <Field as="select" name="orderByReleaseDesc">
-                                <option value={'true'}>Yes</option>
-                                <option value={'false'}>No</option>
+              <FormItem>
+                <div>Name (exact name): </div>
+                <Field type="text" name="name" />
+              </FormItem>
+              <FormItem>
+                <div>Release year: </div>
+                <Field type="number" name="releaseYear" Min={0} />
+              </FormItem>
+              <FormItem>
+                <div>Amount of series: </div>
+                <Field type="number" name="amountOfSeries" Min={0} />
+              </FormItem>
+              <FormItem>
+                <div>Order by release </div>
+                <Field as="select" name="orderByReleaseDesc">
+                  <option value={'true'}>Yes</option>
+                  <option value={'false'}>No</option>
                 </Field>
-            </FormItem>
-            <FormItem>
-              <div>Order by amount of series </div>
-              <Field as="select" name="orderByAmountOfSeries">
-                                <option value={'true'}>Yes</option>
-                                <option value={'false'}>No</option>
+              </FormItem>
+              <FormItem>
+                <div>Order by amount of series </div>
+                <Field as="select" name="orderByAmountOfSeriesDesc">
+                  <option value={'true'}>Yes</option>
+                  <option value={'false'}>No</option>
                 </Field>
-            </FormItem>
-            <FormItem>
-              <Button type="submit" disabled={false}>
-                Submit
-              </Button>
-            </FormItem>
+              </FormItem>
+              <FormItem>
+                <Button type="submit" disabled={false}>
+                  Submit
+                </Button>
+              </FormItem>
             </SearchForm>
           </Form>
         )}
